6e6934eafae299c71e7c1e14c8560f8e53f9ea4d
jni23 <jni23@bath.ac.uk>
2023-11-16 16:22:55 +0000

    Refactor login authentication to enhance security by storing and verifying hashed passwords using BCrypt in the Customer class and updating the NewBank class.

--- server/NewBank.java
+++ server/NewBank.java
@@ -13,15 +13,15 @@ public class NewBank {
 	}
 	
 	private void addTestData() {
-		Customer bhagy = new Customer();
+		Customer bhagy = new Customer("password1");
 		bhagy.addAccount(new Account("Main", 1000.0));
 		customers.put("Bhagy", bhagy);
-		
-		Customer christina = new Customer();
+
+		Customer christina = new Customer("password2");
 		christina.addAccount(new Account("Savings", 1500.0));
 		customers.put("Christina", christina);
-		
-		Customer john = new Customer();
+
+		Customer john = new Customer("password3");
 		john.addAccount(new Account("Checking", 250.0));
 		customers.put("John", john);
 	}
@@ -29,10 +29,13 @@ public class NewBank {
 	public static NewBank getBank() {
 		return bank;
 	}
-	
+
 	public synchronized CustomerID checkLogInDetails(String userName, String password) {
-		if(customers.containsKey(userName)) {
-			return new CustomerID(userName);
+		if (customers.containsKey(userName)) {
+			Customer customer = customers.get(userName);
+			if (BCrypt.checkpw(password, customer.getPasswordHash())) {
+				return new CustomerID(userName);
+			}
 		}
 		return null;
 	}
