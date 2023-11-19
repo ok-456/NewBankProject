79bbb664741cf8a2b400b55dce9015058ce25872
jni23 <jni23@bath.ac.uk>
2023-11-19 18:29:06 +0000

    user can enter wrong password, server will re-prompt for password and will no longer spam null to the user for any wrong password

--- newbank/server/NewBankClientHandler.java
+++ newbank/server/NewBankClientHandler.java
@@ -6,56 +6,60 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-public class NewBankClientHandler extends Thread{
-	
+
+
+public class NewBankClientHandler extends Thread {
 	private NewBank bank;
 	private BufferedReader in;
 	private PrintWriter out;
-	
-	
+
 	public NewBankClientHandler(Socket s) throws IOException {
 		bank = NewBank.getBank();
 		in = new BufferedReader(new InputStreamReader(s.getInputStream()));
 		out = new PrintWriter(s.getOutputStream(), true);
 	}
-	
+
+
 	public void run() {
-		// keep getting requests from the client and processing them
-		try {
-			// ask for user name
-			out.println("Enter Username");
-			String userName = in.readLine();
-			// ask for password
-			out.println("Enter Password");
-			String password = in.readLine();
-			out.println("Checking Details...");
-			// authenticate user and get customer ID token from bank for use in subsequent requests
-			CustomerID customer = bank.checkLogInDetails(userName, password);
-			// if the user is authenticated then get requests from the user and process them 
-			if(customer != null) {
-				out.println("Log In Successful. What do you want to do?");
-				while(true) {
-					String request = in.readLine();
-					System.out.println("Request from " + customer.getKey());
-					String responce = bank.processRequest(customer, request);
-					out.println(responce);
-				}
-			}
-			else {
-				out.println("Log In Failed");
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		finally {
+		boolean loginSuccessful = false;
+
+		while (!loginSuccessful) {
 			try {
-				in.close();
-				out.close();
+				// Ask for user name
+				out.println("Enter Username");
+				String userName = in.readLine();
+				// Ask for password
+				out.println("Enter Password");
+				String password = in.readLine();
+				out.println("Checking Details...");
+
+				String loginResult = bank.checkLogInDetails(userName, password);
+
+				// Authenticate user and get customer ID token from the bank for use in subsequent requests
+				if (loginResult.contains("Login successful")) {
+					out.println("Log In Successful. What do you want to do?");
+					loginSuccessful = true;
+
+					while (true) {
+						String request = in.readLine();
+						System.out.println("Request from " + userName);
+						String response = bank.processRequest(new CustomerID(userName), request);
+
+						// Check for null response
+						if (response != null) {
+							out.println(response);
+						} else {
+							out.println("Server error: Null response");
+							// Break out of the loop if the server response is null
+							break;
+						}
+					}
+				} else {
+					out.println("Login failed: " + loginResult);
+				}
 			} catch (IOException e) {
 				e.printStackTrace();
-				Thread.currentThread().interrupt();
 			}
 		}
 	}
-
 }
