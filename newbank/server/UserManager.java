7f35547211470b4f56c38a88acfe50ee18f70d8d
jni23 <jni23@bath.ac.uk>
2023-11-20 23:27:07 +0000

    code comment for recent code admendment

--- newbank/server/UserManager.java
+++ newbank/server/UserManager.java
@@ -3,16 +3,25 @@ package newbank.server;
 import java.io.*;
 import java.util.Map;
 
+
+/**
+ * UserManager class handles the loading and saving of user data from/to a text file.
+ * Enables new user accounts as well as adding new bank accounts to current users in the system.
+ */
 public class UserManager {
 
     //Text file that allows new user accounts as well as adding new accounts to existing users
     private static final String USER_DATA_FILE = "user_data.txt";
 
+    /**
+     * Loads user data from the text file and populates the provided Map with Customer objects.
+     * @param users The Map to populate with user data
+     */
     public void loadUserData(Map<String, Customer> users) {
         try {
             File file = new File(USER_DATA_FILE);
 
-            // Create a new file if it doesn't exist
+            // Create new user_data txt file if it doesn't exist
             if (!file.exists()) {
                 file.createNewFile();
                 return;
@@ -27,7 +36,7 @@ public class UserManager {
                         String password = userData[1];
                         Customer newCustomer = new Customer(password);
 
-                        // If there are additional account details, add them to the customer
+                        // If there are additional account details i.e different bank account, add them to the customer
                         if (userData.length > 2) {
                             for (int i = 2; i < userData.length; i++) {
                                 String[] accountData = userData[i].split(",");
@@ -38,7 +47,7 @@ public class UserManager {
                                 }
                             }
                         }
-
+                        // Put new customer in the Map
                         users.put(username, newCustomer);
                     }
                 }
@@ -48,6 +57,10 @@ public class UserManager {
         }
     }
 
+    /**
+     * Saves user data from the provided Map to the user_data.txt file.
+     * @param users The Map containing user data to be saved
+     */
     public void saveUserData(Map<String, Customer> users) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(USER_DATA_FILE))) {
             for (Map.Entry<String, Customer> entry : users.entrySet()) {
@@ -55,7 +68,7 @@ public class UserManager {
                 String password = entry.getValue().getPassword(); // Get plaintext password
                 writer.write(username + ":" + password);
 
-                // If the customer has accounts, write them to the file
+                // If customer has accounts, write them to the file
                 for (Account account : entry.getValue().getAccounts()) {
                     writer.write(":" + account.getName() + "," + account.getBalance());
                 }
@@ -67,22 +80,4 @@ public class UserManager {
         }
     }
 
-    public boolean checkUserExists(String username, String password) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(USER_DATA_FILE))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] userData = line.split(":");
-                if (userData.length == 3) {
-                    String storedUsername = userData[0];
-                    String storedPassword = userData[1];
-                    if (storedUsername.equals(username) && storedPassword.equals(password)) {
-                        return true;
-                    }
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
 }
