c116cf1f4917d6c013675941bf70ee97ea6a58a9
jni23 <jni23@bath.ac.uk>
2023-11-20 21:01:54 +0000

    removed password hashing, user details are now stored in plaintext and saved in txt file

--- newbank/server/NewBank.java
+++ newbank/server/NewBank.java
@@ -1,13 +1,14 @@
 package newbank.server;
-import java.util.ArrayList;
 import java.util.HashMap;
 
 public class NewBank {
-	
+
 	private static final NewBank bank = new NewBank();
-	private HashMap<String,Customer> customers;
-	
+	private UserManager userManager;
+	private HashMap<String, Customer> customers;
+
 	private NewBank() {
+		userManager = new UserManager();
 		customers = new HashMap<>();
 		addTestData();
 	}
@@ -27,39 +28,86 @@ public class NewBank {
 		john.addAccount(new Account("Savings", 1500.0));
 		customers.put("John", john);
 	}
-	
+
 	public static NewBank getBank() {
 		return bank;
+	}
+
+	public Map<String, Customer> getCustomers() {
+		return customers;
 	}
 
 	public synchronized String checkLogInDetails(String userName, String password) {
 		if (customers.containsKey(userName)) {
 			Customer customer = customers.get(userName);
-			if (customer.getPasswordHash().equals(hashPassword(password))) {
+			if (customer.getPassword().equals(password)) {
 				return "Login successful. Welcome, " + userName + "!";
 			} else {
 				return "Incorrect password for user " + userName + ". Please try again.";
 			}
 		} else {
-			return "User " + userName + " not found. Would you like to register? (yes/no)";
+			// Check if the user exists in the loaded user data
+			if (userManager.checkUserExists(userName, password)) {
+				return "Login successful. Welcome, " + userName + "!";
+			} else {
+				return "User " + userName + " not found. Would you like to register? (yes/no)";
+			}
 		}
 	}
 
-	// New method for user registration
+	// NewBank class
 	public synchronized String registerUser(String userName, String password) {
 		if (customers.containsKey(userName)) {
 			return "Registration failed: Username already exists.";
 		} else {
 			Customer newCustomer = new Customer(password);
+			newCustomer.setPassword(password); // Set plaintext password
+
 			customers.put(userName, newCustomer);
 			return "Registration successful. Welcome, " + userName + "!";
 		}
 	}
 
-	
-	// commands from the NewBank customer are processed in this method
 
 
+	private String showMyAccounts(CustomerID customer) {
+		if (customers.containsKey(customer.getKey())) {
+			if (customers.get(customer.getKey()).accountsToString().isEmpty()) {
+				return "NO ACCOUNTS";
+			} else {
+				return customers.get(customer.getKey()).accountsToString();
+			}
+		} else {
+			return "FAIL";  // or an appropriate message
+		}
+	}
+
+	private String addNewAccount(CustomerID customer, String[] arguments) {
+		String addNewAccountResult;
+
+		String name;
+		if (arguments.length > 0) {
+			name = arguments[0];
+		} else {
+			return "FAIL";
+		}
+
+		Customer accountHolder = customers.get(customer.getKey());
+
+		if (accountHolder == null || name == null || name.isEmpty()) {
+			return "FAIL";
+		}
+
+		if (accountHolder.hasAccount(name)) {
+			return "FAIL";
+		}
+
+		accountHolder.addAccount(new Account(name, 0.00));
+
+		// Save the updated user data
+		userManager.saveUserData(customers);
+
+		return "SUCCESS";
 
 	}
 }
---
+++ newbank/server/UserManager.java
@@ -0,0 +1,88 @@
+package newbank.server;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserManager {
+
+    private static final String USER_DATA_FILE = "user_data.txt";
+
+    public void loadUserData(Map<String, Customer> users) {
+        try {
+            File file = new File(USER_DATA_FILE);
+
+            // Create a new file if it doesn't exist
+            if (!file.exists()) {
+                file.createNewFile();
+                return; // No need to proceed further as the file is new and empty
+            }
+
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    String[] userData = line.split(":");
+                    if (userData.length >= 2) {
+                        String username = userData[0];
+                        String password = userData[1];
+                        Customer newCustomer = new Customer(password);
+
+                        // If there are additional account details, add them to the customer
+                        if (userData.length > 2) {
+                            for (int i = 2; i < userData.length; i++) {
+                                String[] accountData = userData[i].split(",");
+                                if (accountData.length == 2) {
+                                    String accountName = accountData[0];
+                                    double balance = Double.parseDouble(accountData[1]);
+                                    newCustomer.addAccount(new Account(accountName, balance));
+                                }
+                            }
+                        }
+
+                        users.put(username, newCustomer);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void saveUserData(Map<String, Customer> users) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(USER_DATA_FILE))) {
+            for (Map.Entry<String, Customer> entry : users.entrySet()) {
+                String username = entry.getKey();
+                String password = entry.getValue().getPassword(); // Get plaintext password
+                writer.write(username + ":" + password);
+
+                // If the customer has accounts, write them to the file
+                for (Account account : entry.getValue().getAccounts()) {
+                    writer.write(":" + account.getName() + "," + account.getBalance());
+                }
+
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean checkUserExists(String username, String password) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(USER_DATA_FILE))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] userData = line.split(":");
+                if (userData.length == 3) {
+                    String storedUsername = userData[0];
+                    String storedPassword = userData[1];
+                    if (storedUsername.equals(username) && storedPassword.equals(password)) {
+                        return true;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
