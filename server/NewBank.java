e251b6182bfe894e454be0377af0d16b9e44aaa0
jni23 <jni23@bath.ac.uk>
2023-11-29 00:11:49 +0000

    merging exit implementation and check login as one

--- server/NewBank.java
+++ server/NewBank.java
@@ -2,27 +2,31 @@ package newbank.server;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
 public class NewBank {
 	
 	private static final NewBank bank = new NewBank();
+
+	private UserManager userManager;
 	private HashMap<String,Customer> customers;
 	
 	private NewBank() {
+		userManager = new UserManager();
 		customers = new HashMap<>();
 		addTestData();
 	}
 	
 	private void addTestData() {
-		Customer bhagy = new Customer();
+		Customer bhagy = new Customer("password1");
 		bhagy.addAccount(new Account("Main", 1000.0));
 		customers.put("Bhagy", bhagy);
-		
-		Customer christina = new Customer();
+
+		Customer christina = new Customer("password2");
 		christina.addAccount(new Account("Savings", 1500.0));
 		customers.put("Christina", christina);
-		
-		Customer john = new Customer();
+
+		Customer john = new Customer("password3");
 		john.addAccount(new Account("Checking", 250.0));
 		john.addAccount(new Account("Main", 1000.0));
 		john.addAccount(new Account("Savings", 1500.0));
@@ -32,12 +36,40 @@ public class NewBank {
 	public static NewBank getBank() {
 		return bank;
 	}
-	
-	public synchronized CustomerID checkLogInDetails(String userName, String password) {
-		if(customers.containsKey(userName)) {
-			return new CustomerID(userName);
+
+	// Returns customer map
+	public Map<String, Customer> getCustomers() {
+		return customers;
+	}
+
+
+	// Checks login details
+	public synchronized String checkLogInDetails(String userName, String password) {
+		if (customers.containsKey(userName)) {
+			Customer customer = customers.get(userName);
+			if (customer.getPassword().equals(password)) {
+				return "Login successful. Welcome, " + userName + "!";
+			} else {
+				return "Incorrect password for user " + userName + ". Please try again.";
+			}
+		} else {
+
+			return "User " + userName + " not found. Would you like to register? (yes/no)";
+
 		}
-		return null;
+	}
+
+	public synchronized String registerUser(String userName, String password) {
+		if (!customers.containsKey(userName)) {
+			// Create a new customer and add it to the customers HashMap
+			Customer newCustomer = new Customer(password);
+			newCustomer.setPassword(password); // Set plaintext password
+
+			customers.put(userName, newCustomer);
+			return "Registration successful. Welcome, " + userName + "!";
+		} else {
+			return "Registration failed: User already exists. Please choose a different username.";
+		}
 	}
 
 
@@ -53,6 +85,8 @@ public class NewBank {
 			case "SHOWMYACCOUNTS" : return showMyAccounts(customer);
 			case "NEWACCOUNT": return addNewAccount(customer, arguments);
 			case "MOVE": return moveMoney(customer, arguments);
+			case "EXIT": return null;
+
 			default : return "FAIL";
 			}
 		}
@@ -88,6 +122,9 @@ public class NewBank {
 		if(accountHolder.hasAccount(name)) {
 			return "FAIL";
 		}
+
+		// Save the updated user data
+		userManager.saveUserData(customers);
 
 		accountHolder.addAccount(new Account(name, 0.00));
 
